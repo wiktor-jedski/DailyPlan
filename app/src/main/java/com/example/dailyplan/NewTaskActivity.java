9eb764d0da291997e1b7c9e550bf2632e8e24639
wiktor-jedski <jedrzejewskiwiktor@gmail.com>
2020-12-01 16:56:52 +0100

    added buttons in NewTaskActivity

--- app/src/main/java/com/example/dailyplan/NewTaskActivity.java
+++ app/src/main/java/com/example/dailyplan/NewTaskActivity.java
@@ -3,26 +3,44 @@ package com.example.dailyplan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class NewTaskActivity extends AppCompatActivity {
 
     private EditText mEditTitle;
     private EditText mEditPriority;
+    private int priority = 0;
+    private int clicked = 0;
     public static final String EXTRA_TITLE = "com.example.android.dailyplan.TITLE";
     public static final String EXTRA_PRIORITY = "com.example.android.dailyplan.PRIORITY";
+    private ImageButton button_A;
+    private ImageButton button_B;
+    private ImageButton button_C;
+    private ImageButton button_D;
+    private ImageButton button_E;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
         mEditTitle = findViewById(R.id.titleEditText);
-        mEditPriority = findViewById(R.id.priorityEditText);
+        //mEditPriority = findViewById(R.id.priorityEditText);
 
+        // find priority buttons
+        button_A = findViewById(R.id.buttonA);
+        button_B = findViewById(R.id.buttonB);
+        button_C = findViewById(R.id.buttonC);
+        button_D = findViewById(R.id.buttonD);
+        button_E = findViewById(R.id.buttonE);
+
+        // implement save button
         Button button = findViewById(R.id.buttonSave);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,7 +58,39 @@ public class NewTaskActivity extends AppCompatActivity {
                 finish();
             }
         });
+
     }
 
-    // implement spinner
+    public void onPriorityButtonClick(View view) {
+        Log.i("buttons", Integer.toString(view.getId()));
+        restoreDefaultButtons();
+        setImageButtonPriority(view);
+    }
+
+    public void restoreDefaultButtons() {
+        button_A.setImageDrawable(getDrawable(R.drawable.ic_priority_a));
+        button_B.setImageDrawable(getDrawable(R.drawable.ic_priority_b));
+        button_C.setImageDrawable(getDrawable(R.drawable.ic_priority_c));
+        button_D.setImageDrawable(getDrawable(R.drawable.ic_priority_d));
+        button_E.setImageDrawable(getDrawable(R.drawable.ic_priority_e));
+    }
+
+    public void setImageButtonPriority(View view) {
+        if (view == button_A) {
+            button_A.setImageDrawable(getDrawable(R.drawable.ic_priority_a_clicked));
+        }
+        else if (view == button_B) {
+            button_B.setImageDrawable(getDrawable(R.drawable.ic_priority_b_clicked));
+        }
+        else if (view == button_C) {
+            button_C.setImageDrawable(getDrawable(R.drawable.ic_priority_c_clicked));
+        }
+        else if (view == button_D) {
+            button_D.setImageDrawable(getDrawable(R.drawable.ic_priority_d_clicked));
+        }
+        else {
+            button_E.setImageDrawable(getDrawable(R.drawable.ic_priority_e_clicked));
+        }
+    }
 }
+
